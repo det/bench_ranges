2ed51e71e2c8a6a3c5f47160e3574231dd0ebaeb
Chris Clearwater <chris@detrino.org>
2014-10-12 20:47:52 -0700

    use std algorithms, and non-deprecated shuffle

--- bench.hpp
+++ bench.hpp
@@ -19,15 +19,18 @@ void bench(int argc, char **argv, Functor functor, Args && ... args)
     int count = atoi(argv[1]);
 
     std::unique_ptr<int[]> data(new int[count]);
-    auto rng = ranges::view::counted(data.get(), count);
-    ranges::iota(rng, 0);
-    ranges::random_shuffle(rng);
+    auto first = data.get();
+    auto last = first + count;
+
+    std::iota(first, last, 0);
+    std::default_random_engine engine;
+    std::shuffle(first, last, engine);
 
     auto start = std::chrono::high_resolution_clock::now();
     functor(data, count, std::forward<Args>(args) ...);
     auto stop = std::chrono::high_resolution_clock::now();
 
-    if (!std::is_sorted(data.get(), data.get() + count)) throw std::runtime_error{"Insertion sort failed"};
+    if (!std::is_sorted(first, last)) throw std::runtime_error{"Insertion sort failed"};
 
     auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
     std::cout << ns.count() / 1000000.0 << "\n";    
